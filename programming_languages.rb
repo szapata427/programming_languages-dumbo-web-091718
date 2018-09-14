44cf90dc407fb341a70730c65c4da9571c6d3322
Steven Zapata <szapata427@gmail.com>
2018-09-14 03:08:44 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,21 +1,30 @@
-def reformat_languages(languages)
-new_hsh = {}
 
+
+
+require "pry"
+def reformat_languages(languages)
+  
+  new_hash = {}
   languages.each do |style, lang_hsh|
     var = style
-    
+  # binding.pry
+  
     lang_hsh.each do |language, type|
-      
-      if new_hsh.include? (language)
-        
-        new_hsh[language][:style].push(var)
-        
+       #binding.pry
+      if new_hash.include? (language)
+        new_hash[language][:style] << var
+       
       else
-        new_hsh[language] = type
-        new_hsh[language][:style] = [var]
-        
+        new_hash[language] = type
+        new_hash[language][:style] = [var]
       end
     end
   end
-  new_hsh
+  new_hash
 end
+
+# {:oo=>{:ruby=>{:type=>"interpreted"}, :javascript=>{:type=>"interpreted"}, :python=>{:type=>"interpreted"}, :java=>{:type=>"compiled"}}, :functional=>{:clojure=>{:type=>"compiled"}, :erlang=>{:type=>"compiled"}, :scala=>{:type=>"compiled"}, :javascript=>{:type=>"interpreted"}}}
+
+
+
+# {:ruby=>{:type=>"interpreted", :style=>[:oo]}, :javascript=>{:type=>"interpreted", :style=>[:oo, :functional]}, :python=>{:type=>"interpreted", :style=>[:oo]}, :java=>{:type=>"compiled", :style=>[:oo]}, :clojure=>{:type=>"compiled", :style=>[:functional]}, :erlang=>{:type=>"compiled", :style=>[:functional]}, :scala=>{:type=>"compiled", :style=>[:functional]}}
